ea5e3857484f6d7afa887717927cff429847df7f
Devon Le Duke <lede1011@hs-karlsruhe.de>
2013-12-19 23:06:19 +0100

    methode zum map aufruf implementiert

---
+++ EventsLocatorClient/src/de/hska/eb/eventslocatorclient/EventProfile.java
@@ -0,0 +1,38 @@
+package de.hska.eb.eventslocatorclient;
+
+import java.util.List;
+
+import android.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class EventProfile extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_eventProfile);
+		
+	}
+	
+	public void onClickPlace(View view) {
+		TextView tv = (TextView) findViewById(R.id.event_place);
+		Uri location = Uri.parse("geo:0,0?q="+tv.getText());
+		Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+		
+		PackageManager packageManager = getPackageManager();
+		List<ResolveInfo> activities = packageManager.queryIntentActivities(mapIntent, 0);
+		boolean isIntentSafe = activities.size() > 0;
+		
+		if (isIntentSafe) {
+		    startActivity(mapIntent);
+		}
+	}
+	
+}
--- EventsLocatorClient/src/de/hska/eb/eventslocatorclient/Overview.java
+++ EventsLocatorClient/src/de/hska/eb/eventslocatorclient/Overview.java
@@ -8,7 +8,7 @@ public class Overview extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_login);
+		setContentView(R.layout.activity_overview);
 		
 	}
 	
